a78ba5f6f34e0cb3ea70cc87c0d55776af785e3b
ShreyasP7 <poojaryshreys432@gmail.com>
2021-09-29 00:37:21 +0530

    Bot Connected With Backend

---
+++ kisan-mitra-frontend/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCToJNsvj-D0r2IS7bhwsiff5FEN4vvj2A",
+    authDomain: "kisan-mitra-e9137.firebaseapp.com",
+    projectId: "kisan-mitra-e9137",
+    storageBucket: "kisan-mitra-e9137.appspot.com",
+    messagingSenderId: "929184811678",
+    appId: "1:929184811678:web:d27ca5b277b1ec5b92898f",
+    measurementId: "G-V1Q5BKV9G6"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  
+
+  export {db};
