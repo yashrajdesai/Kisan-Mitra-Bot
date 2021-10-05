9a856bf5c255e3738ae70434527a68e682926b57
Mihir <ranemihir3@gmail.com>
2021-10-05 16:00:41 +0530

    Completed backend of chat bot

--- kisan-mitra-frontend/src/components/Home/Home.js
+++ kisan-mitra-frontend/src/components/Home/Home.js
@@ -1,28 +1,60 @@
-import React, { useState } from 'react'
-import bot from "./bot.png"
+import React, { useEffect, useState } from 'react'
 import { IconButton } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 import axios from 'axios';
+import { collection, query, orderBy, onSnapshot, addDoc, Timestamp } from "firebase/firestore"; 
+import {firestore as db} from "../../firebase.js";
 
+import bot from "./bot.png"
 import "./Home.css"
+
 
 function Home() {
 
+    const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
     const [botMessage, setBotMessage] = useState("");
 
-    const sendMessage = async () => {
+    useEffect(() => {
+        const q = query(collection(db, "users/7y5BmQyWY93YRJBgWfIM/chats"), orderBy("timestamp", "desc"));
 
-        const jsonMessage = JSON.stringify({ message });
-        console.log(jsonMessage);
-        const res = await axios.post('http://localhost:3001/sendMessage', {body : jsonMessage}, {
-            headers: {
-                'Content-Type': 'application/json'
-            }
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const chats  = [];
+            querySnapshot.forEach((doc) => {
+                chats.push(doc.data());
+            });
+
+            setMessages(chats);
+            
         });
-        console.log(res.data);
-        setBotMessage(res.data.answer)
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const sendMessage = async (e) => {
+
+            e.preventDefault();
+
+            const jsonMessage = JSON.stringify({ message });
+
+            axios.post('http://localhost:3001/sendMessage', {body : jsonMessage}, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(async (res) => {
+                setBotMessage(res.data.answer)
+                await addDoc(collection(db, 'users/7y5BmQyWY93YRJBgWfIM/chats'), {
+                    botAnswer: res.data.answer,
+                    userMessage: message,
+                    timestamp: Timestamp.now()
+                });
+
+                setMessage("")
+            });
+       
     }
 
     return (
@@ -35,30 +67,41 @@ function Home() {
 
                 <div className="chat-body">
 
-                    <p className="chat-message">
-                        <span className="chat-name">Kisan Mitra Bot</span>
-                        <span className="chat-text">{botMessage}</span>
-                    </p>
+                    {
+                        messages.map((msg) => (
+                            <>
+                                <p className="chat-message">
+                                    <span className="chat-name">Kisan Mitra Bot</span>
+                                    <span className="chat-text">{msg.botAnswer}</span>
+                                    <br />
+                                    <span className="chat-timestamp">{new Date(msg.timestamp?.toDate()).toLocaleString("en-US", {timeZone: 'Asia/Kolkata'})}</span>
+                                </p> 
 
-                    {(message.length != 0) && <p className="chat-message chat-receiver">
-                        <span className="chat-name">You</span>
-                        <span className="chat-text">{message}</span>
-                    </p>
+                                <p className="chat-message chat-receiver">
+                                    <span className="chat-name">You</span>
+                                    <span className="chat-text">{msg.userMessage}</span>
+                                    <br />
+                                    <span className="chat-timestamp">{new Date(msg.timestamp?.toDate()).toLocaleString("en-US", {timeZone: 'Asia/Kolkata'})}</span>
+                                </p> 
+                            </>
+                        ))
                     }
+
                 </div>
                 
                 <div className="chat-footer">
-                    <IconButton className="icon-button">
-                        <MicIcon style={{fill: "white"}}/>
-                    </IconButton>
+                    <form>
+                        <IconButton className="icon-button">
+                            <MicIcon style={{fill: "white"}}/>
+                        </IconButton>
 
-                    <form >
                         <input value = {message} onChange = {(e)=>{setMessage(e.target.value);}} placeholder="Type a message" type="text" />                      
+                        
+                        <IconButton className="icon-button">
+                            <button type="submit" id="messageSubmit" onClick={sendMessage}>Send a message</button>
+                            <label htmlFor="messageSubmit"><SendIcon style={{fill: "white"}}/></label>
+                        </IconButton>
                     </form>
-
-                    <IconButton className="icon-button" onClick = {sendMessage}>
-                        <SendIcon style={{fill: "white"}}/>
-                    </IconButton>
                 </div>
             </div>
 
