1181505d83fe9059f2f221713dfbec3db8cd713e
Mihir <ranemihir3@gmail.com>
2021-10-11 19:39:01 +0530

    Added authentication in website

--- kisan-mitra-frontend/src/components/Home/Home.js
+++ kisan-mitra-frontend/src/components/Home/Home.js
@@ -4,8 +4,11 @@ import SendIcon from '@material-ui/icons/Send';
 import MicIcon from '@material-ui/icons/Mic';
 import axios from 'axios';
 import { collection, query, orderBy, onSnapshot, addDoc, Timestamp } from "firebase/firestore"; 
+import { useHistory } from 'react-router-dom';
+
 import {firestore as db} from "../../firebase.js";
 import bot from "./bot.png"
+import { useStateValue } from "../../StateProvider";
 import "./Home.css"
 
 
@@ -14,44 +17,61 @@ function Home() {
     const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
     const [botMessage, setBotMessage] = useState("");
+    const [{userId,user},dispatch] = useStateValue();
+
+    const history = useHistory()
 
     useEffect(() => {
-        const q = query(collection(db, "users/7y5BmQyWY93YRJBgWfIM/chats"), orderBy("timestamp", "desc"));
 
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const chats  = [];
-            querySnapshot.forEach((doc) => {
-                chats.push(doc.data());
+        var unsubscribe = null;
+
+        if(user){
+
+            const q = query(collection(db, "users/"+userId+"/chats"), orderBy("timestamp", "desc"));
+
+            unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const chats  = [];
+                querySnapshot.forEach((doc) => {
+                    chats.push(doc.data());
+                });
+
+                setMessages(chats);
+                
             });
-
-            setMessages(chats);
-            
-        });
+        }
 
         return () => {
-            unsubscribe();
+            if(unsubscribe){
+                unsubscribe();
+            }
         }
-    }, [])
+    }, [user,userId])
 
     const sendMessage = async (e) => {
 
             e.preventDefault();
-            const jsonMessage = JSON.stringify({ message });
-            axios.post('http://localhost:3001/sendMessage', {body : jsonMessage}, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then(async (res) => {
-                console.log(res.data.answer);
-                setBotMessage(res.data.answer);
-                await addDoc(collection(db, 'users/7y5BmQyWY93YRJBgWfIM/chats'), {
-                    botAnswer: res.data.answer,
-                    userMessage: message,
-                    timestamp: Timestamp.now()
+            if(user){
+                const jsonMessage = JSON.stringify({ message });
+                axios.post('http://localhost:3001/sendMessage', {body : jsonMessage}, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(async (res) => {
+                    console.log(res.data.answer);
+                    setBotMessage(res.data.answer);
+                    await addDoc(collection(db, "users/"+userId+"/chats"), {
+                        botAnswer: res.data.answer,
+                        userMessage: message,
+                        timestamp: Timestamp.now()
+                    });
+
+                    setMessage("")
                 });
 
-                setMessage("")
-            });
+            }else{
+                alert("You must be signed in to chat with our bot")
+                history.push("/login")
+            }
        
     }
 
@@ -76,7 +96,7 @@ function Home() {
                                 </p> 
 
                                 <p className="chat-message chat-receiver">
-                                    <span className="chat-name">You</span>
+                                    <span className="chat-name">{user.Name}</span>
                                     <span className="chat-text">{msg.userMessage}</span>
                                     <br />
                                     <span className="chat-timestamp">{new Date(msg.timestamp?.toDate()).toLocaleString("en-US", {timeZone: 'Asia/Kolkata'})}</span>
